8cf84a24465a598e8c360859c01ab6c35b2f7669
1070344 <1070344@isep.ipp.pt>
2013-10-04 20:18:51 +0100

    Actualização de classes
    actualização da classe LocaisHistoricosCulturais.h.

--- MapaDigital/MapaDigital/LocaisHistoricosCulturais.h
+++ MapaDigital/MapaDigital/LocaisHistoricosCulturais.h
@@ -18,14 +18,37 @@ class LocaisHistoricosCulturais : public Locais
 		int tempoVisita;//tempo medio de visita em minutos
 		int horarioAbertura;//em minutos
 		int horarioEncerramento;//em minutos
+	
 	public:
 		LocaisHistoricosCulturais();
 		LocaisHistoricosCulturais(const LocaisHistoricosCulturais &locHist);
 		LocaisHistoricosCulturais(string desc,int tempoVisita,int horarioAbertura,int horarioEncerramento);
 		~LocaisHistoricosCulturais();
 
+		//SET's e GET's
+		void setTempoVisita(int tVisita);
+		void setHorarioAbertura(int abertura);
+		void setHorarioEncerramento(int encerramento);
 
+		int getTempoVisita()const;
+		int getHorarioAbertura()const;
+		int getHorarioEncerramento()const;
+
+		
 };
+
+LocaisHistoricosCulturais::LocaisHistoricosCulturais()
+{
+	tempoVisita = 0;
+	horarioAbertura = 0;
+	horarioEncerramento = 0;
+}
+
+LocaisHistoricosCulturais::LocaisHistoricosCulturais(const LocaisHistoricosCulturais &locHist)
+{
+
+}
+						
 
 
 #endif
